edbb27fa43cc8c727104fd14310ea3455ed4b691
Takapon0407 <takapon_business@yahoo.co.jp>
2021-02-22 01:12:23 +0900

    クエリ用のフィールド（いったん自分の情報だけ取得）を用意して、それで情報の取得ができるようにする

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
-console.log(GITHUB_TOKEN);
+import React, { Component } from "react";
+import client from "./client";
+import { ApolloProvider } from "react-apollo";
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
 
-function App() {
-  return <div>Hello, GraphQL</div>;
+const ME = gql`
+  query me {
+    user(login: "Takapon0407") {
+      name
+      avatarUrl
+    }
+  }
+`;
+
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <div>Hello, GraphQL</div>
+
+        <Query query={ME}>
+          {({ loading, error, data }) => {
+            if (loading) return "Loading...";
+            if (error) return `Error! ${error.message}`;
+
+            return <div>{data.user.name}</div>;
+          }}
+        </Query>
+      </ApolloProvider>
+    );
+  }
 }
 
 export default App;
