85ac40a6784772b3981aa73d2f1d66f72b26061b
Takapon0407 <takapon_business@yahoo.co.jp>
2021-02-25 08:17:23 +0900

    戻るボタンを実装

--- src/App.js
+++ src/App.js
@@ -41,8 +41,18 @@ class App extends Component {
     });
   }
 
+  goPrevious(search) {
+    this.setState({
+      first: null,
+      after: null,
+      last: PER_PAGE,
+      before: search.pageInfo.startCursor,
+    });
+  }
+
   render() {
     const { query, first, last, before, after } = this.state;
+    console.log({ query });
     return (
       <ApolloProvider client={client}>
         <form onSubmit={this.handleSubmit}>
@@ -81,6 +91,11 @@ class App extends Component {
                   })}
                 </ul>
 
+                {search.pageInfo.hasPreviousPage === true ? (
+                  <button onClick={this.goPrevious.bind(this, search)}>
+                    Previous
+                  </button>
+                ) : null}
                 {search.pageInfo.hasNextPage === true ? (
                   <button onClick={this.goNext.bind(this, search)}>Next</button>
                 ) : null}
