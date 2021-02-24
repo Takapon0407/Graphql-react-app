8d3b719a12f9378eec78d2f40c1dc28897643005
Takapon0407 <takapon_business@yahoo.co.jp>
2021-02-25 01:30:07 +0900

    フォームを作成後、入力された情報でqueryを更新して、結果を取得できるように修正

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Query } from "react-apollo";
 import { ME } from "./graphql";
 import { SEARCH_REPOSITORIES } from "./graphql";
 
-const VARIABLES = {
+const DEFAULT_STATE = {
   first: 5,
   after: null,
   last: null,
@@ -16,7 +16,9 @@ const VARIABLES = {
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = VARIABLES;
+    this.state = DEFAULT_STATE;
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
@@ -32,8 +34,12 @@ class App extends Component {
 
   render() {
     const { query, first, last, before, after } = this.state;
+    console.log({ query });
     return (
       <ApolloProvider client={client}>
+        <form onSubmit={this.handleSubmit}>
+          <input value={query} onChange={this.handleChange} />
+        </form>
         <Query
           query={SEARCH_REPOSITORIES}
           variables={{ query, first, last, before, after }}
